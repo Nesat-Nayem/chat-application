8621b2b8d09dd1dc164c05045b10d63278383f02
NesatNayem <nesatnayem042@gmail.com>
2022-11-14 21:02:28 +0600

    fast commit

---
+++ router/inboxRouter.js
@@ -0,0 +1,38 @@
+// external imports
+const express = require("express");
+
+// internal imports
+const {
+  getInbox,
+  searchUser,
+  addConversation,
+  getMessages,
+  sendMessage,
+} = require("../controller/inboxController");
+const decorateHtmlResponse = require("../middlewares/common/decorateHtmlResponse");
+const { checkLogin } = require("../middlewares/common/checkLogin");
+const attachmentUpload = require("../middlewares/inbox/attachmentUpload");
+
+const router = express.Router();
+
+// inbox page
+router.get("/", decorateHtmlResponse("Inbox"), checkLogin, getInbox);
+// router.get("/", decorateHtmlResponse("Inbox"),  getInbox);
+
+// search user for conversation
+router.post("/search", checkLogin, searchUser);
+// router.post("/search", searchUser);
+
+// add conversation
+router.post("/conversation", checkLogin, addConversation);
+// router.post("/conversation", addConversation);
+
+// get messages of a conversation
+router.get("/messages/:conversation_id", checkLogin, getMessages);
+// router.get("/messages/:conversation_id",  getMessages);
+
+// send message
+router.post("/message", checkLogin, attachmentUpload, sendMessage);
+// router.post("/message",  attachmentUpload, sendMessage);
+
+module.exports = router;
